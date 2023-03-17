5480cd837f7c6959425377f75d8febfc79de5e03
Noelle Huchette <noelle.huchette@gmail.com>
2023-03-16 20:51:33 -0400

    list article component

---
+++ components/ListedArticle.js
@@ -0,0 +1,51 @@
+import { Card, Typography, Box } from '@mui/material';
+import Image from './image';
+import Link from 'next/link';
+import { getStrapiMedia } from '@/lib/media';
+
+const ListedArticle = ({ article }) => {
+  let capitalized = article.attributes.title
+    .split(' ')
+    .map((word) => {
+      return word[0].toUpperCase() + word.substring(1);
+    })
+    .join(' ');
+  let category = article.attributes.category.data.attributes.name;
+  console.log(article);
+  let date = new Date(article.attributes.publishedAt);
+  return (
+    <Card
+      sx={{
+        m: 1,
+        display: 'flex',
+        alignItems: { xs: '', sm: 'center' },
+        gap: 2,
+        flexDirection: { xs: 'column', sm: 'row' },
+        textAlign: {}
+      }}
+    >
+      <Box sx={{ width: { xs: '100%', sm: '30%' } }}>
+        <Link href={`/article/${article.attributes.slug}`}>
+          <Image image={article.attributes.image} />
+        </Link>
+      </Box>
+      <Box
+        sx={{ display: 'flex', flexDirection: 'column', p: { xs: 1, sm: 0 } }}
+      >
+        <Link href={`/category/${category}`}>
+          <Typography variant="subtitle2">{category.toUpperCase()}</Typography>
+        </Link>
+        <Link href={`/article/${article.attributes.slug}`}>
+          <Typography variant="h5">{capitalized}</Typography>
+        </Link>
+        <Box>
+          <Typography variant="caption">
+            {date.toDateString().toUpperCase()}
+          </Typography>
+        </Box>
+      </Box>
+    </Card>
+  );
+};
+
+export default ListedArticle;
