1c92a6719b9ce02e64d8424e6feb16347097d470
Fiona Murray <fiona.c.murray@gmail.com>
2023-04-28 21:39:35 -0400

    small fix

--- components/Homepage/card.js
+++ components/Homepage/card.js
@@ -20,10 +20,10 @@ export const Card = ({ article }) => {
   return (
     <div key={article.id} className={styles.card}>
       <Paper className={styles.article} style={{ margin: '10px' }}>
-        <Link href={`/article/${article.attributes.slug}`}><a>
+        <Link href={`/article/${article.attributes.slug}`}>
           <Typography className={styles.header} variant="h6">
             {capitalized}
-          </Typography></a>
+          </Typography>
           <Image className={styles.image} image={article.attributes.image} />
         </Link>
         <div className={styles.bottom}>
